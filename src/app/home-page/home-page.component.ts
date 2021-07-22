bdbb102aa093ead011b29d68e2da68cb2e5b5e43
jmfx <jumanji.felix@gmail.com>
2021-07-22 19:30:41 +0200

    pulsanti servizi

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faUserMd, faVial, faFileMedical, faUserCog } from '@fortawesome/free-solid-svg-icons';
-
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -11,6 +11,7 @@ export class HomePageComponent implements OnInit {
   faDoc = faUserMd;
   faLab = faVial;
   faMedLav = faUserCog;
+  faCircle = faCircle;
 
   constructor() { }
 
