8b25b61a9f34e5e28832d730da7437cb3701901a
jmfx <jumanji.felix@gmail.com>
2021-07-28 10:16:33 +0200

    pagina lab-analisi

--- src/app/lab-analisi/lab-analisi.component.ts
+++ src/app/lab-analisi/lab-analisi.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabAnalisiComponent implements OnInit {
 
+  branche : string[] = [
+    "Laboratorio Generale di Base",
+    "Chimica Clinica",
+    "Cito Istopatologia",
+    "Ematologia",
+    "Immunosierologia",
+    "Microbiologia",
+    "Tossicologia"
+  ]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/med-lav.service.ts
+++ src/app/med-lav.service.ts
@@ -7,11 +7,11 @@ import { MedLavInfo } from './med-lav-info';
 })
 export class MedLavService {
 
-  private apiUrl: string = 'https://60e7113c15387c00173e4a54.mockapi.io/';
+  private apiUrl: string = 'https://60e7113c15387c00173e4a54.mockapi.io/richiesteML/';
 
   constructor(private http: HttpClient) { }
 
   postRichiestaMedLav( mdInfo : MedLavInfo) {
-    return this.http.post<MedLavInfo>(this.apiUrl + 'richiesteML/', mdInfo);
+    return this.http.post<MedLavInfo>(this.apiUrl, mdInfo);
   }
 }
